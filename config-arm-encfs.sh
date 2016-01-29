a94153fbca837e23e76b16864812dd511e46fce2
Christoph Schmidt-Hieber <christoph.schmidthieber@googlemail.com>
2016-01-29 22:03:36 +0000

    Upgrade openssl to 1.0.2f

--- config-arm-encfs.sh
+++ config-arm-encfs.sh
@@ -18,7 +18,7 @@
 
 TOOLCHAIN=${HOME}/android-toolchain
 FUSEDIR=`pwd`/../../fuse293
-OPENSSLDIR=`pwd`/../../openssl/openssl-1.0.2e
+OPENSSLDIR=`pwd`/../../openssl/openssl-1.0.2f
 BOOSTDIR=`pwd`/../../boost/boost_1_60_0
 RLOGDIR=`pwd`/../../rlog/rlog-1.4
 MYAR=${TOOLCHAIN}/bin/arm-linux-androideabi-ar
